ce07a3b28c2a5827332728600b8b4d3dc89195d3
DipuKP2001 <dipukp602@gmail.com>
2020-11-28 12:33:12 +0530

    fetch url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { useState,useEffect } from 'react'
 import Map from './components/Map'
  
 function App() {
+
+  const [eventData,setEventData] = useState([])
+  const [loading,setLoading] = useState(false)
+
+  useEffect(()=>{
+    const fetchEvents = async() => {
+      setLoading(true)
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const { events } = await res.json()
+      setEventData(events)
+      setLoading(false)
+    }
+    fetchEvents() 
+    console.log(eventData);
+  },[])
+
   return (
     <div className="App">
       <Map />
