d07cb71534fe9e5da9134b07c42a7d266401c7c8
DipuKP2001 <dipukp602@gmail.com>
2020-11-28 18:52:14 +0530

    loaction-info styles completed

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ function App() {
   const [eventData,setEventData] = useState([])
   const [loading,setLoading] = useState(false)
 
-  useEffect(()=>{
-    const fetchEvents = async() => {
+  useEffect(() => {
+    const fetchEvents = async () => {
       setLoading(true)
       const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
       const { events } = await res.json()
+
       setEventData(events)
       setLoading(false)
     }
-    fetchEvents() 
-  },[])
+
+    fetchEvents()
+  }, [])
 
   return (
     <div>
